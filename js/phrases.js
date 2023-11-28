5882c1865f0480082a9aa3da89c5586e7384c78a
aBgantsovaDoe <117314039+aBgantsovaDoe@users.noreply.github.com>
2023-11-29 01:02:53 +0300

    BIG UPDATE

--- js/phrases.js
+++ js/phrases.js
@@ -1,72 +1,77 @@
-const RUS_PHRASES = [
-  'Привычка - вторая натура',
-  'Заметьте хорошо!',
-  'Беда не приходит одна',
-  'Через тернии к звёздам',
+// Массивы латинских высказываний и их переводов
+var latinPhrases = [
+  "\"Consuetudo est altera natura\"",
+  "\"Nota bene\"",
+  "\"Nulla calamitas sola\"",
+  "\"Per aspera ad astra\""
 ];
-const LATIN_PHRASES = [
-  'Consuetudo est altera natura',
-  'Nota bene',
-  'Nulla calamitas sola',
-  'Per aspera ad astra',
+var translationPhrases = [
+  "\"Привычка - вторая натура\"",
+  "\"Заметьте хорошо!\"",
+  "\"Беда не приходит одна\"",
+  "\"Через тернии к звёздам\""
 ];
-const board = document.querySelector('.board');
-const createButton = document.querySelector('.create');
 
-function getRandomInt(max) {
-  max = Math.floor(max);
-  return Math.floor(Math.random() * max);
-};
-let used = new Array();
-function getPhrase () {
-  let num;
-  let different;
-  do {
-    different = true;
-    num = getRandomInt(RUS_PHRASES.length);
-    for (let el in used) {
-      if (used[el] === num) different = false;
-    }
+// Классы для фонового цвета
+var class1 = "background-color1";
+var class2 = "background-color2";
+
+// Переменная для отслеживания нажатий на кнопку
+var counter = 0;
+
+// Функция для создания нового элемента при нажатии на кнопку "Создать"
+function createNewElement() {
+
+  var randomIndex = Math.floor(Math.random() * latinPhrases.length);
+
+  if (latinPhrases.length === 0) {
+    alert("Фразы закончились");
+    return;
   }
-  while (!different)
-  used[used.length] = num;
-  return RUS_PHRASES[num];
-};
 
-const template = document.querySelector('#phrase-template');
+  var element = document.createElement("p");
+  element.id = "new-element-" + counter;
+   
+  var elementNumber = document.createElement("span");
+  elementNumber.style.textDecoration = "underline";
+  elementNumber.textContent = "n=" + counter;
+  element.appendChild(elementNumber);
+  
+  var latinElement = document.createElement("span");
+  latinElement.style.fontStyle = "italic";
+  latinElement.textContent = " " + latinPhrases[randomIndex];
+  element.appendChild(latinElement);
+   
+  var russianElement = document.createElement("span");
+  russianElement.style.fontStyle = "normal";
+  russianElement.textContent =  " " + translationPhrases[randomIndex];
+  element.appendChild(russianElement);
 
-const onCreateClick = function () {
-  if (used.length === RUS_PHRASES.length) {
-    document.querySelector('.warning').classList.remove('hidden');
+  latinPhrases.splice(randomIndex, 1);
+  translationPhrases.splice(randomIndex, 1);
+  
+  var randDiv = document.getElementById("rand");
+  
+  // Устанавливаем класс для фона в зависимости от счетчика
+  if (counter % 2 === 0) {
+    element.classList.add(class1);
+  } else {
+    element.classList.add(class2);
   }
-  else {
-    const item = template.content.cloneNode(true);
-    item.querySelector('.phrase__text').textContent = `${used.length + 1}. ${getPhrase()}`;
-    board.appendChild(item);
-  }
-};
+  randDiv.appendChild(element);
+  counter++;
+}
 
-createButton.addEventListener('click', onCreateClick);
-
-board.addEventListener('click', function (evt) {
-  const item = evt.target;
-  item.closest('.phrase').classList.add('onclick');
-  let text = item.textContent;
-  const num = text.substring(0, text.indexOf('.'));
-  item.textContent = `${num}. ${LATIN_PHRASES[used[num - 1]]}`;
-  const parent = item.closest('.phrase').classList.add('move');
-  parent.classList.add('move');
-
-});
-
-const colorButton = document.querySelector('.color');
-colorButton.addEventListener('click', function () {
-  const items = board.querySelectorAll('.phrase');s
-  if (items.length > 0) {
-    for (let el = 0; el < items.length; el++) {
-      if(items[el].classList.contains('move')) {
-        items[el].style.backgroundColor = 'rgb(0, 251, 255)';
+// Функция для изменения жирности при нажатии на кнопку
+function changeBackground() {
+  const container = document.querySelector("#rand");
+  const matches = container.querySelectorAll("p");
+  if (matches.length > 0) {
+    for (let el = 0; el < matches.length; el++) {
+      if(!(el % 2)) {
+        matches[el].style.fontWeight = "bold";
       }
     }
-  }  
-});
+  } 
+}
+
